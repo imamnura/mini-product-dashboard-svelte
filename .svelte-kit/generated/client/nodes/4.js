fad40d8043aa2cc1b2c7afa4736f28020fe4027d
imamnura <imam.arifin@bluebirdgroup.com>
2026-07-13 02:45:24 +0700

    remove svelte-kit generate

--- .svelte-kit/generated/client/nodes/4.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/product/[id]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/product/[id]/+page.svelte";
